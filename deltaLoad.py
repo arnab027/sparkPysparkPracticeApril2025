0c962c04925e25ad57e80272677d52e92c73b71d
Arnab Dutta <arnab.dutta027@gmail.com>
2025-04-21 04:04:25 -0700

    pyspark delta Lake

---
+++ deltaLoad.py
@@ -0,0 +1,48 @@
+import pyspark
+from delta import *
+
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
+    .config('spark.sql.debug.maxToStringFields', 2000) \
+    .config('spark.debug.maxToStringFields', 2000)
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+data = spark.range(0, 5)
+data.write.format("delta").mode("overwrite").save("/tmp/delta-table")
+
+df = spark.read.format("delta").load("/tmp/delta-table")
+df.show()
+
+data = spark.range(5, 10)
+data.write.format("delta").mode("overwrite").save("/tmp/delta-table")
+
+df.show()
+
+from delta.tables import *
+from pyspark.sql.functions import *
+
+deltaTable = DeltaTable.forPath(spark, "/tmp/delta-table")
+
+# Update every even value by adding 100 to it
+deltaTable.update(
+  condition = expr("id % 2 == 0"),
+  set = { "id": expr("id + 100") })
+
+# Delete every even value
+deltaTable.delete(condition = expr("id % 2 == 0"))
+
+# Upsert (merge) new data
+newData = spark.range(0, 20)
+
+deltaTable.alias("oldData") \
+  .merge(
+    newData.alias("newData"),
+    "oldData.id = newData.id") \
+  .whenMatchedUpdate(set = { "id": col("newData.id") }) \
+  .whenNotMatchedInsert(values = { "id": col("newData.id") }) \
+  .execute()
+
+deltaTable.toDF().show()
+
