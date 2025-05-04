2f5851820fcfd6f5b4bb812acadd35f2323869e4
Arnab Dutta <arnab.dutta027@gmail.com>
2025-05-04 09:08:27 +0530

    updated pyspark path

--- pyspark_code/mysqlDataFrameLoad.py
+++ pyspark_code/mysqlDataFrameLoad.py
@@ -6,7 +6,7 @@ builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
     .config('spark.sql.debug.maxToStringFields', 2000) \
     .config('spark.debug.maxToStringFields', 2000)\
-    .config("spark.jars", "D:\\pysparkProject\\pysparkPracticeApril2025\\mysql-connector-j-9.3.0.jar")
+    .config("spark.jars", "D:\\pysparkProject\\pysparkPracticeApril2025\\pyspark_code\\mysql-connector-j-9.3.0.jar")
 
 spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
@@ -23,3 +23,14 @@ df.write\
     .option("user", "root") \
     .option("password", "root") \
     .save()
+
+mysql_df = spark.read \
+                .format("jdbc") \
+                .option("driver","com.mysql.cj.jdbc.Driver") \
+                .option("url", "jdbc:mysql://localhost:3306/PySparkTables") \
+                .option("dbtable", "DestinationTable1") \
+                .option("user", "root") \
+                .option("password", "root") \
+                .load()
+
+mysql_df.show()
