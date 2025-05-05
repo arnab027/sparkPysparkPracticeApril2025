4d2ec8561e2f176a040b7d7541c92e401e5c6c7a
Arnab Dutta <arnab.dutta027@gmail.com>
2025-05-05 19:38:12 +0530

    python generator and generator from  with  example

---
+++ pythonPractice/pythonGenerator.py
@@ -0,0 +1,29 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def generate_number(number_limit:int)->iter:
+    if isinstance(number_limit,int) and number_limit > 0:
+        for numbers_between in range(0, number_limit):
+            logger.info(msg="Inside generate_number")
+            yield numbers_between
+
+
+def call_generator_function(number_limit:int)->iter:
+    generator_calling_instance = generate_number(number_limit)
+    # print(next(generator_calling_instance))
+    # print(next(generator_calling_instance))
+    if next(generator_calling_instance) <number_limit:
+        logger.info(msg="Inside call_generator_function")
+        yield from generator_calling_instance
+
+values_fetched = call_generator_function(6)
+generator_val = iter(values_fetched)
+
+while True:
+    try:
+        value = next(generator_val)
+    except StopIteration:
+        break
+    print(value)
